698a00ce00a254b1d8619887582fab62eb94b445
Gabriel Dias <125761843+Gab-Dias@users.noreply.github.com>
2023-10-18 21:30:54 -0300

    Add files via upload

---
+++ JavaScript/Aula6/exe.js
@@ -0,0 +1,14 @@
+
+let timesFutebol = ['Palmeiras', 'Santos', 'Flamengo', 'Botafogo', 'Vasco'];
+
+timesFutebol.pop()
+console.log(timesFutebol);
+
+timesFutebol.shift()
+console.log(timesFutebol);
+
+timesFutebol.push('Criciuma')
+console.log(timesFutebol);
+
+timesFutebol.unshift("Vitoria");
+console.log(timesFutebol);
